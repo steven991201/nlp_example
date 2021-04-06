45e4679d8836688b4923f481afb434d1d2493784
steven <steven@kaist.ac.kr>
2021-04-06 17:55:40 +0900

    Update CNN.py

--- start/CNN.py
+++ start/CNN.py
@@ -32,8 +32,7 @@ def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-    matplotlib.pyplot.show()
+    plt.show(block=True)
 
 
 # 학습용 이미지를 무작위로 가져오기
